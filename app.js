d42fa4e81112cade3e6aa7a31354621dbef8e2e7
dirkliu <liuqi0702@126.com>
2017-06-30 22:53:10 +0800

    app.js server

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-/**
- * 静态文件服务器
- */
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
 http.createServer((req, res) => {
-   // console.log('url req:',  url.parse(req.url))
    var pathname = url.parse(req.url).pathname;
+   var fileType=pathname.split('.').pop()
+
    fs.readFile(path.join(__dirname, pathname), function (err, file) {
         if (err) {
             res.writeHead(404, {"Content-Type": "text/html;charset:utf-8"});
             res.end('404,Not found!');
             return;
         }
-        res.writeHead(200, {"Content-Type": "text/html;charset:utf-8"});
+
+        switch (fileType) {
+          case 'html':
+          case 'htm':
+            res.writeHead(200, {"Content-Type": "text/html;charset:utf-8"});
+            break;
+          case 'txt':
+            res.writeHead(200, {"Content-Type": "text/plain;charset:utf-8"});
+            break;
+        }
         res.end(file);
     });
     console.log('Static server is running! ROOT:', __dirname);
