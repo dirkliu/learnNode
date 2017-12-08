a6f69777b8df6cb1f80e7fb14d7c33fdd52778ce
dirkliu <liuqi0702@126.com>
2017-12-08 23:00:19 +0800

    node.js download!

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const http = require('http');
 const url = require('url');
 const fs = require('fs');
 const path = require('path');
-const routes = require('routes');
+const routes = require('./routes');
 
 http.createServer((req, res) => {
    var pathname = url.parse(req.url).pathname;
    var fileType=pathname.split('.').pop()
 
+   if(pathname.indexOf('/download/' === 0)) {
+     // res.set({
+     //   'Content-type': 'application/octet-stream',
+     //   'Content-Disposition': 'attachment;filename=download.text'
+     // })
+     res.setHeader('Content-type', 'application/octet-stream');
+     res.setHeader('Content-Disposition', 'attachment;filename=download.text');
+     fs.createReadStream(path.join(__dirname, 'README.md')).pipe(res)
+     return
+   }
    fs.readFile(path.join(__dirname, pathname), function (err, file) {
         if (err) {
             res.writeHead(404, {"Content-Type": "text/html;charset:utf-8"});
