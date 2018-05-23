031005089f6cfb5eb6bc5108a2faa0c631530641
dirkliu <liuqi0702@126.com>
2018-05-23 20:23:55 +0800

    net模块测试！

--- net.js
+++ net.js
@@ -1,2 +1,20 @@
-const os = require("os")
-console.log('os.networkInterfaces:', os.networkInterfaces())
+// net 模块提供了创建基于流的 TCP 或 IPC 服务器(net.createServer())和客户端(net.createConnection()) 的异步网络 API。
+const net = require('net')
+
+const server = net.createServer((socket) => {
+  socket.end('goodbye\n');
+}).on('error', (err) => {
+  // handle errors here
+  throw err;
+}).on('close', err => {
+  console.log('soket closed')
+});
+
+// grab an arbitrary unused port.
+server.listen(() => {
+  console.log('opened server on', server.address());
+});
+
+server.getConnections((err, count) => {
+  console.log('connections:', count)
+})
